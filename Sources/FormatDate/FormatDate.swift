95ff4334d9f2df035c373b7038fef81de5ef570d
GiorgiAmiranashvili <giorgi.amiranashvili6@gmail.com>
2024-11-13 16:13:00 +0400

    Initial Commit

---
+++ Sources/FormatDate/FormatDate.swift
@@ -0,0 +1,26 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public protocol FormatDateProtocol {
+    func formatingDate(T: String) -> String
+}
+
+public class FormatDate {
+    
+    init() { }
+    
+    public func formatingDate(T: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+
+        if let date = dateFormatter.date(from: T) {
+            dateFormatter.dateFormat = "EEEE, dd MMMM yyyy"
+            let formattedDate = dateFormatter.string(from: date)
+            return formattedDate
+        } else {
+            return ""
+        }
+    }
+}
